cabc2e6e1aac5658ee2c3b00e958031180de9cea
Nicola Boyle <nicola162@gmail.com>
2023-01-31 22:23:58 +0000

    added if statements to JS to differentiate past/future/present on time blocks

--- assets/script.js
+++ assets/script.js
@@ -8,9 +8,33 @@ const currentHours = moment().hour();
 
 var timeDisplayEl = $("#currentDay");
 var today = moment();
+
+// Timer function
+
 setInterval(displayTime, 1000);
 function displayTime() {
     var currentTime = moment().format("dddd Do MMM YYYY");
     timeDisplayEl.text(currentTime);
 }
 
+
+
+time.each(function(i, element){
+let elementTime = Number(element.id);
+
+if (elementTime === currentHours) {
+    $(element).children(".description")
+.addClass("present");
+} else if (elementTime > currentHours) {
+    $(element).children(".description").addClass("future");
+    } else {
+        $(element).children(".description").addClass("past");
+    }
+
+    let hour = $(element).first().text().trim();
+    let description = localStorage.getItem(hour);
+
+    if(description) {
+        $(element).children(".description").val(description);
+    }
+});
