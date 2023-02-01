ff680af903a3007b1cae549ed5e6fb218ae9d02a
Nicola Boyle <nicola162@gmail.com>
2023-02-01 12:39:41 +0000

    added code to change colour attributes for past/future/present time blocks

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,8 @@
-// Declare consts
-
-const submitBtn = $(".saveBtn");
-const time = $(".time-block");
-const currentHours = moment().hour();
-
-// Display the current date
+// Display the current date and check for previously saved user input upon page loading
 
 var timeDisplayEl = $("#currentDay");
-var today = moment();
+var textInputs = $('.description');
+renderText();
 
 // Timer function
 
@@ -17,24 +12,52 @@ function displayTime() {
     timeDisplayEl.text(currentTime);
 }
 
+// Get text fields by class and compare current time block with text area ID for each time block
 
+const rows = document.getElementsByClassName("description");
+var currentHour = parseInt(moment().format('H'));
 
-time.each(function(i, element){
-let elementTime = Number(element.id);
+// Select colour attributes according to current time of day
 
-if (elementTime === currentHours) {
-    $(element).children(".description")
-.addClass("present");
-} else if (elementTime > currentHours) {
-    $(element).children(".description").addClass("future");
+for (var i=0; i<rows.length; i++){
+    var compareHours = rows[i].id;
+    var selectColor = rows[i];
+    if(compareHours < currentHour){
+        setColor.setAttribute('class', 'past col-10 description');
+    } else if(compareHours > currentHour){
+        setColor.setAttribute('class', 'future col-10 description');
     } else {
-        $(element).children(".description").addClass("past");
+        setColor.setAttribute('class', 'present col-10 description');
     }
 
-    let hour = $(element).first().text().trim();
-    let description = localStorage.getItem(hour);
 
-    if(description) {
-        $(element).children(".description").val(description);
-    }
-});
+    // time.each(function(i, element){
+    // let elementTime = Number(element.id);
+
+    // if (elementTime === currentHours) {
+    //     $(element).children(".description")
+    // .addClass("present");
+    // } else if (elementTime > currentHours) {
+    //     $(element).children(".description").addClass("future");
+    //     } else {
+    //         $(element).children(".description").addClass("past");
+    //     }
+
+    //     let hour = $(element).first().text().trim();
+    //     let description = localStorage.getItem(hour);
+
+    //     if(description) {
+    //         $(element).children(".description").val(description);
+    //     }
+    // });
+
+    // $('.saveBtn').on('click', function(){
+    //     var hourSaved = parseIn(this.id);
+    //     var textToSave = getText(hourSaved);
+    //     var saveObject = {
+    //         time: hourSaved,
+    //         description: textToSave
+    //     }
+    //     updateLocalMemory(saveObject);
+    // });
+
